d8f05214c4fd509c14244c52cbdb286004726042
Tudor <tbrindus@gmail.com>
2016-08-16 14:19:45 -0400

    Report width/height of images in response

--- texoid/server.py
+++ texoid/server.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import urllib
+import re
 
 import subprocess
 
@@ -15,6 +16,8 @@ from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
 
+redimensions = re.compile('.*?(\d+)x(\d+).*?')
+
 LATEX_PATH = os.environ.get('LATEX_BIN', '/usr/bin/latex')
 DVISVGM_PATH = os.environ.get('DVISVGM_BIN', '/usr/bin/dvisvgm')
 CONVERT_PATH = os.environ.get('CONVERT_BIN', '/usr/bin/convert')
@@ -26,13 +29,21 @@ for path in [LATEX_PATH, DVISVGM_PATH, CONVERT_PATH]:
 
 
 def svg_to_png(svg):
-    comp = subprocess.Popen([CONVERT_PATH, 'svg:-', 'png:/dev/stdout'], stdout=subprocess.PIPE,
+    comp = subprocess.Popen([CONVERT_PATH, '-identify', 'svg:-', 'png:-'], stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     out, err = comp.communicate(svg)
     if comp.returncode:
         print >> sys.stderr, err
         raise AssertionError('convert exited with error code')
-    return out
+    first_nl = out.find('\n')
+    ident = out[:first_nl]
+
+    dim = redimensions.match(ident)
+    width = dim.group(1)
+    height = dim.group(2)
+
+    out = out[first_nl + 1:]
+    return out, width, height
 
 
 def dvi_to_svg(filename):
@@ -68,11 +79,14 @@ class MainHandler(tornado.web.RequestHandler):
 
                 filename = latex_to_dvi(raw.name)
                 svg_data = dvi_to_svg(filename).strip()
-                png_data = svg_to_png(svg_data).strip().encode('base64')
+                png_data, width, height = svg_to_png(svg_data)
+                png_data.strip().encode('base64')
                 return {
                     'success': True,
                     'svg': svg_data,
-                    'png': png_data
+                    'png': png_data,
+                    'width': width,
+                    'height': height
                 }
         except Exception as error:
             print error.message
