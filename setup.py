494cf81e97695151a6f5af424355d8457d1baa83
Quantum <quantum2048@gmail.com>
2018-12-18 23:43:58 -0500

    Prepare for release v0.2

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
+
 from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='texoid',
@@ -15,15 +20,20 @@ setup(
     author_email='quantum@dmoj.ca',
     url='https://github.com/DMOJ/texoid',
     description='LaTeX math rendering server.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
-        'Development Status :: 3 - Beta',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Education',
+        'Topic :: Artistic Software',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
         'Topic :: Software Development',
     ],
 )
